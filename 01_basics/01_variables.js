8bb0fea7b20a9d48598986a42528a802fa679993
pranjal-sharma-web <pranjalsharma3842@gmail.com>
2024-12-12 11:16:13 +0000

    variables and datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountID = 144553
+let accountEmail = "pranjal@gamil.com"
+var accountPassword ="232323"
+let accountState;
+
+accountCity = "jaipur"
+
+// accountID ="1313133333333"
+accountEmail = "hasfkl@gamil.com"
+accountCity =   "bhopal"
+accountPassword = "dfsdfdfdfd"
+
+accountCity = "sehore"
+accountEmail = "pranjal@com"
+accountPassword = "adfdfdf"
+
+// please dont use VAR beacuse of issuse in block scope and function
+
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+console.log(accountID);
+
+accountEmail = "hsasn@gamil.com"
+accountPassword = "vvvlllvvllvv"
+accountCity = "indore"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; 
+// treate all js as newer version
+
+// allert (3+3) we are using nodejs, not browser
+
+
+let name = "pranjal"
+let age =  "19"
+let isLoggedIn = true/false
+let state = null
+
+// number = range 2 to 53/52
+// bigint = large number
+// string =  name
+// boolean = true/false
+// null =  "standalone value "
+// undefined = "if value connot defined"
+// symbol = unique 
+
+// object 
+
+console.log(typeof undefined) //undefined//
+console.log(typeof null)// type object//
+
