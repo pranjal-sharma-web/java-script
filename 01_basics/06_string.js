41eff13d6d0fd81917bf51e0ec51ed000544d240
pranjal-sharma-web <pranjalsharma3842@gmail.com>
2024-12-28 11:04:36 +0000

    datatype

---
+++ 01_basics/06_string.js
@@ -0,0 +1,695 @@
+// const name = "pranjal"
+// let surname = "sharma"
+// console.log(`hello my name is ${name} and my surname is ${surname}`);
+
+// one more  formula
+
+// const gamename = new String("pranjal-ps")
+
+// console.log(gamename.__proto__);
+
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(6));
+// console.log(gamename.indexOf('a'));
+// console.log(gamename.(''));
+// console.log(gamename);
+
+// const newString = gamename.substring(0,5)
+// console.log(newString);
+
+// const  anotherstring = gamename.slice(-6,5)
+// console.log(anotherstring);
+
+
+
+// const newStringone = "   pranjal   "
+
+// console.log(newStringone);
+// console.log(newStringone.trim());
+
+
+
+
+// const url = "https://youtube.com/pranjal%20sharma"
+// console.log(url.replace('%20', '-'))
+
+
+
+// let newString = new String("pranjal-sharma-ps")
+
+// let Str = newString.blink('')
+
+// console.log(newString)
+// console.log(newString.replace('%20,','-'))
+
+// console.log(Str[11])
+
+// const array1 = [5, 12, 8, 130, 44,49,30,40,20];
+
+// let index = 6;
+
+// console.log(`an index of ${index} and return value is ${array1.at(index)}`);
+
+// const str = 'my name is pranjal sharma';
+// let index = 4;
+// console.log(`character code is ${str.charCodeAt(index)},is equal to ${str.charAt(index)}`);
+
+// const str = 'my name is pranjal sharma';
+// let index = 1;
+// console.log(`character code is ${str.charCodeAt(index)}, is equal to ${str.charAt(index)}`);
+
+// let array1 = [1,2,3,]
+// let array2 = [4,5,6,]
+// let array3 = [array1.concat(array2)]
+// console.log(array3);
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// constructor methood to set initial value of the object
+
+
+// class name1 {
+//     constructor(){
+//         this.name = "pranjal"
+//         this.age =  19
+//     }
+
+
+// }
+// let name2 = new name1()
+// console.log(name2.name,name2.age);
+
+// class cars1 {
+//     constructor(){
+//         this.car = "bmw"
+//         this.model = 2001
+//         this.price = "20lakh"
+// nod
+// }
+
+
+
+// class bike {
+//     constructor(){
+//         this.bike = "mt"
+//         this.rate = 2.5
+//     }
+// }
+
+// let bike1 = new bike()
+// console.log(bike1.bike, bike1.rate);
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// let cars2 = new cars1()
+// console.log(cars2.car,cars2.model,cars2.price)
+
+
+
+
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// ends withe methode 
+
+// const str1 = "i am lear coding"
+// console.log(str1.endsWith("coding"));
+
+
+// let str2 = "what are you doing"
+// console.log(str2.endsWith("what"));
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+
+
+// function financil(x){
+//     return Number.parseFloat(x).toFixed(2)
+// }
+
+// console.log(financil(12.3224));
+
+
+    
+
+// zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+// let array8 = [1,2,3,4,5,6,67,7]
+// console.log(array8.includes(67));
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// const array1 = ['DOg' ,'tiger', 'bmw', 'mercedies','tiger',]
+
+// console.log(array1.indexOf('tiger',5));
+
+// ###################################################################33
+// is well formed
+
+
+// let jsonString = {"name":"pranjal" , "age" : 19}
+
+// console.log(jsonString);
+
+// ######################################################################
+
+// localcompare     stringmethod
+
+// let fruits = "bannana"
+// let fruits1 = fruits.localeCompare("bannana")
+
+// console.log(fruits1);
+
+// ########################################################################
+
+// let str = "my name is pranjal sharma ";
+// let string = str.matchAll()
+// console.log(string);
+
+
+
+// #########################################################################
+
+// search method string
+
+// let about = "pranjal sharma is very good boy and he is very innocent and honest"
+// console.log(about.search("sharma"));
+
+// ##########################################################################
+
+// splite
+
+// let bmw5 = "bmw, lamborghini, pagani ,meserati"
+// console.log(bmw5.split(","));
+
+// ########################################################################
+
+// padstart / and padend
+
+
+// let musics = "2"
+// console.log(musics.padEnd(4,"000"));
+
+// // let '
+
+// let song = "2"
+// console.log(song.padStart(2,"00"));
+
+
+// ###########################################################################
+
+// let num1 = [1023,37,39,50,60,69]
+// let index = 0
+// console.log(`the value is ${index} return value is ${num1.at(index)}`);
+
+
+// ###############################################################################3
+// fixe method is number method
+
+
+// let imp = 30.000
+// console.log(imp.toFixed(4));
+
+
+// #####################################################################################
+
+// slice is both arry and string to find the portion of the value
+
+// let bool =  "hallo how are you"
+// let bool1 = bool.slice(-4)
+// console.log(bool1);
+
+// #####################################################################################
+//  this is the replace method of String
+
+// let sum = "hii sharmaji"
+// console.log(sum.replace("sharmaji","pranjal"));
+
+
+// ######################################################################################3
+
+// let search = "this is the string in javascript"
+// console.log(search.search("javascript"));
+
+// ##############################################################
+
+// startwith method in js
+
+// let laptop = "Hallo my laptop is asus"
+// console.log(laptop.startsWith("Hallo"));
+// console.log(laptop.startsWith("my",6));
+// ################################################################
+
+// substing method fo js
+
+// let true1 = "godzilla , mountain"
+// let false1 = true1.substring(0,17
+
+// )
+// console.log(false1);
+// #########################################################################
+
+// tolocallowercasr
+// let userName = "pranjal"
+// console.log(`PS : ${userName.toLocaleLowerCase('ps')}`);
+
+
+// let password = "pranjal1221"
+// console.log(`PW : ${password.toLocaleLowerCase('pw')}`);
+
+// ##########################################################################
+
+// tolocal uppercase
+
+// let yourName = "sharma"
+// console.log(` : ${yourName.toLocaleUpperCase.apply('sharma')}`);
+// 
+// ###################################################################################################################################
+// practice.
+
+// at 
+
+// let myName = "pranjal sharma ps "
+// let index = 5;
+// console.log(myName.at(index));
+
+// let num = [ 1,2,3,4,4,5,5,5,]
+// let index = 5
+// console.log(`index  ${index} and the return value is ${num.at(index)}`);
+
+// #Z########################
+
+
+// charcodeat unicode
+
+// let yournum = "pranjal sharma my name"
+// let index = 5
+// console.log(`char code of ${yournum.charCodeAt(index)} is ${yournum.charAt(index)}`);
+
+// let myName = "vandana sharma ji"
+// let index = 8;
+// console.log(`character code ${myName.charCodeAt(index)} is equal to ${myName.charAt(index)}`);
+
+
+// ###########################
+
+// code pointat is use for icons
+
+// concat = is merge two string wee add two string in concat 
+
+// let myname = "pranjal"
+// let mysurname = " sharma"
+// console.log(myname.concat("",mysurname));
+// ################################
+
+// let country = "capital of INDIA IS "
+// let capital = "DELHI"
+// console.log(country.concat("",capital));
+
+// #################################
+
+// end with
+
+// let hallo = "hii everyone how are you"
+// console.log(hallo.endsWith("are"));
+
+
+// let mummy = "my mom is very best"
+
+// console.log(mummy.endsWith("best"));
+
+// ################################
+
+// constructo
+
+// class name1 {
+//     constructor() {
+//         this.name = "pranjal"
+        
+//     }
+// }
+
+
+// let name2 = new name1()
+// console.log(name2.name);
+
+// class carsname {
+//     constructor() {
+//         this.carname = "bmw"
+//         this.model = "m5"
+
+//     }
+// }
+
+// let carsname1 = new carsname()
+// console.log(carsname1.carname,carsname1.model);
+
+
+// class cityName {
+//     constructor() {
+//         this.city = "sehore"
+//         this.state = "mp"
+//         this.capital =" Bhopal"
+//     }
+// }
+
+// let city = new cityName()
+// console.log(city.city, city.state, city.capital);
+
+
+// class India {
+//     constructor() {
+//         this.state = "29-state"
+//         this.union = "7 union - terriority"
+        
+//     }
+    
+// }
+
+// let  Bharat = new India()
+// console.log(Bharat.state, Bharat.union);
+
+// #########################################################################3
+
+// to-fixed methode  give decimal  it is used to fixed number of decimal
+
+// let fixed = 12233.3444
+// console.log(fixed.toFixed(2));
+
+
+// const num = 2394.9900
+// console.log(num.toFixed(0));
+
+// ###########################################################
+
+// include know about the word is in the sentance and para
+
+// let name4 = "pranjal sharma"
+// console.log(name4.includes("hallo"));
+
+// let num = [12,34,56,6,7,7,7,]
+// console.log(num.includes(5));
+
+// let para = "the is the black color"
+// let word = "black"
+// console.log(`the word "${word}" ${para.includes(word) ? 'is': "is not" } in the sentances`);
+
+// let capita = "the capital India is Delhi"
+// let word = "indore"
+// console.log(`the "${word}" ${capita.includes(word) ? 'is':"is not"} the capital of India`);
+
+// #################################################
+
+// indexof method of string values searches this string and returns theindex of the first occurannce
+
+// let para = "string has many methods methods"
+// console.log(para.indexOf("method",3));
+
+
+// let array = ['tiger' , 'lion' , 'cat' ,'tiger' , 'dog']
+// console.log(array.indexOf('tiger',1));
+
+
+// let word = "city are very clean"
+// let name = "very"
+// console.log(`the number ${name} is ${word.indexOf(name)} `);
+
+
+// ########################################################
+//  is well formed  is dont understand
+
+// let nameAga = {"name" : "pranjal", "age":1}
+// console.log(nameAga);
+
+// function isWellFormedJSON(str) {
+// try {
+//     JSON.parse(str);
+//     return true;
+// }
+// catch (e) {
+//     return false;
+
+// }
+// }
+
+// console.log(isWellFormedJSON('{"name" : "pranjal", "age":}'));
+
+// one more
+
+// function isWellFormedutf8(str) {
+//     try {
+//         JSON.parse(str)
+//         return true;
+//     }
+//     catch(e){
+//         return false;
+//     }
+// }
+// console.log(isWellFormedutf8('Hello, world!'));
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//  lastIndexOf methode serch the string and return the las occurance
+
+
+// let follow = "hallo please follow my account"
+// console.log(follow.lastIndexOf('my'));
+
+// let pat = "my fav car is mercedies"
+// let word = "mercedies"
+// console.log(`the last index of ${word} is ${pat.lastIndexOf(word)}`)
+
+
+// ##################################################################3
+
+// localcompare 
+
+// let fruits = "apple"
+// let fruits1 = "dog"
+// console.log(fruits1.localeCompare(fruits));
+
+// let pets = 'dog'
+// let pet = 'cat'
+// console.log(pet.localeCompare(pets));
+
+
+// ################################################------------------
+
+// let match = "dog is bite the person"
+// console.log(match.matchAll(String));
+
+
+// let str = "hallo this is prnajal "
+// let string = str.match('is')
+// console.log(string);
+
+// let test = "l love javascript . javascript is best";
+// let result = test.matchAll(/javascript/g);
+// for (let match of result){
+// console.log(match);
+
+
+// let gym = "the best gym . gym  is best"
+// let gyms = gym.matchAll(/gym/g);
+// for (let match of gym){
+//     console.log(match);
+    
+// }
+
+// let gym = "the bull gym. bull is the animal"
+// let gymm = gym.matchAll("bull")
+// for (let match of gym)
+// console.log(match);
+
+// let gym = "i am going daily gym"
+// let gymm = gym.match('going')
+// console.log(gymm);
+
+// ###############################################################
+
+// normallize function
+
+ 
+// let str2 = "e\u0301";
+
+
+
+// console.log(str2);
+// #####################################################
+
+// pad end and pad start method
+
+// padend and padstart are used to give pad and length.
+
+// let name2 = "pranjal";
+// let name1 = name2.padStart(10)
+// console.log(name1);
+
+
+// let howw = "hallo i am pranjal sharma"
+// let howc = howw.slice(-30)
+// let how = howc.padStart(howw.length,"^");
+// console.log(how);
+
+// pad start
+
+// let how = "293848494949"
+// let howw = how.slice(-4)
+// let howww = howw.padStart(how.length,"-")
+
+// console.log(howww);
+
+
+// r###########################################################
+
+// repeat is string method they repeat the string
+
+// let repeat = "hi "
+// console.log(repeat.repeat(3));
+
+
+// #############################################################
+
+// replace is string method it replace 1 to another string
+
+// let replace = "pranjal verma"
+// let rep = replace.replace('verma','sharma')
+// console.log(rep);
+
+
+// ###########################################################
+
+// let rep = "hallo how where you"
+// console.log(rep.replaceAll( 'you' ,'hii what are you doing'));
+
+// let rep = [49404,40,40,40,40]
+// console.log(rep.replaceAll['','12,3,45,56,67']);
+
+// #######################################################
+
+// search method is any string will be search the know about their index
+
+// let sec = "marketing research is my sub"
+// console.log(sec.search('is'));
+
+
+// ###########################################################
+
+// slice mthd in str is used to extract a portion of a string or an array 
+
+// let str = "js is very popular language"
+// console.log(str.slice(-21));
+
+
+// let array = [1,2,3,4,5,5,5,6,6,4,5]
+// console.log(array.slice(-8));
+
+// #########################################################333
+
+// splite .divide divide string into ordered list and give us whic word are in what number
+
+// let str = "mp is the center state in mp"
+// console.log(str.split(''));
+
+// let string = "indore is cleanst city in "
+// let str = string.split(' ')
+// console.log(str[2]);
+
+
+// let string = "hallo this is my laptop of asus"
+// let str = string.split(' ')
+// console.log(str[4]);
+// console.log(str[5]);
+
+
+// ######################################################
+// start with string determin string are start with are true or false
+
+// let you = "you are very beautifull"
+// console.log(you.startsWith('you'));
+
+// ############################################3#
+
+// substring,
+
+// let pg = "amazon is used for shopping"
+// console.log(pg.substring(-9));
+
+// ##################################################
+
+// let country = " america";
+// console.log( `USA: ${country.toLocaleLowerCase('USA')}`);
+
+// let myname = "pranjal-sharma"
+// console.log(`ps: ${myname.toLocaleUpperCase('PS')}`);
+
+
+// ######################################################33
+
+// uppercase - small to capital
+
+// let yourname = "pranjal"
+// console.log(yourname.toLocaleUpperCase());
+
+// ##############################################################3
+
+// to string mthod is use to convert and object num to string
+
+
+// let array = [ 12,2,33,45,67,78]
+// console.log(array.toString(''));
+
+// let num = 2233444555;
+// console.log(num.toString());
+
+// #######################################################
+
+// is well formed
+
+
+
+// function isWellFormedJSON(str) {
+//     try{
+//         JSON.parse(str);
+//         return true
+//     }
+//         catch (e){
+//             return false
+//         }
+    
+// }
+// console.log(isWellFormedJSON({ "name" : "pranjal", "age" : 19}));
+// @#############################################################
+
+// trim remove the white space of strings
+
+// let you = "   pranjal-sharma-ji   "
+// console.log(you.trim());
+// console.log(you);
+
+// let you = "        pranjal "
+// console.log(you.trimStart());
+
+// #############################################################
+
+// const name2 = new String('pranjal')
+// console.log(name2);
+// console.log(name2.valueOf());
+
+// ############################################################
+
+// let str = "hello"
+// let iterator = str[Symbol.iterator]();
+
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+
+
+
