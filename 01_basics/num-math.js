e44995f6d5d1fe1637c3eb06063062b6808f9421
pranjal-sharma-web <pranjalsharma3842@gmail.com>
2024-12-31 15:14:51 +0000

    num-math

---
+++ 01_basics/num-math.js
@@ -0,0 +1,219 @@
+// const score = 500
+// console.log(score);
+
+// const anothernum = new Number(5020)
+// console.log(anothernum);
+
+// // console.log(anothernum.toString().length);
+// console.log(anothernum.toFixed(2));1
+
+// const othernum = 123.39394
+// console.log(othernum.toPrecision(4));
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'));
+
+// const one = [100 ,22, 33, 445 ]
+// console.log(one.valueOf(2));
+
+
+// const two = 346535643
+// console.log(two.toExponential(4));
+
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(500)
+// console.log(balance.toString);
+
+
+// const num = 100
+// console.log(num.toFixed(4));
+
+
+
+// const othernum = 45.456566
+// console.log(othernum.toPrecision(5));
+
+// const othernum = 78.25589
+// console.log(othernum.toPrecision(6));
+
+
+
+// const num = 34.677959
+// console.log(num.toPrecision(7));
+
+// const num = 89.33899
+// console.log(num.toPrecision(6));
+
+// #########################################################
+// to local steing
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'));
+
+// const num = 29384903
+// console.log(num.toLocaleString('en-in'));
+
+
+// ##########################################################################3
+
+// to exponential   
+
+
+// const num = 39.8484834
+// console.log(num.toExponential(4));
+
+// const sum = 566.50606
+// console.log(sum.toExponential(5));
+
+// const sum1 = 3455.40404
+// console.log(sum1.toExponential(5));
+
+
+// const num = 334040.494950
+// console.log(num.toExponential(5));
+
+// const sum = 4565.494940
+// // console.log(sum.toExponential(5));
+
+
+// const score = 3400
+// console.log(score);
+
+// const sum = new String(22233)
+// console.log(sum);
+
+// const value = 2.23444
+// console.log(value.toFixed(8));
+
+// const value2 = 500000
+// console.log(value2.toFixed(4));
+
+// const num = 54.367656
+// console.log(num.toPrecision(6));
+
+// const sum = 43.4459683
+// console.log(sum.toPrecision(8));
+
+// const num = 30000000
+// console.log(num.toLocaleString('en-in'));
+
+// const num = 45.56666
+// console.log(num.toExponential(4));
+
+// const one = 344.345555
+// console.log(one.toExponential(6));
+
+// const num = 45454545
+// console.log(num.valueOf(2));
+
+// #$$#####################################
+// value of
+
+// const num = new Boolean(2234333)
+// console.log(num);
+
+// console.log(num.valueOf());
+
+// let person = {
+//     name:'pranjal',
+//     age: 19,
+//     valueof: function(){
+//         return this.name
+//     }
+    
+    
+// }
+
+// console.log(person.valueof());
+
+// let person1 = {
+//     name: 'pranjalsharma',
+//     college: 'sage-university-bhopal',
+//      function(){
+// return this.college
+//     }
+
+// }
+
+// console.log(person1.valueof());
+
+// let yourname = {
+//     yourname: 'vandana-sharma',
+//     yourage: 45,
+//     city: 'sehore',
+//     dob: '14/12/1980',
+//    function(){
+//     return this.yourage,
+//     this.yourname,
+//     this.city,
+//     this.dob
+//    },
+
+// }
+// console.log(yourname);
+
+// const detail = {
+//     name:'pranjal',
+//     age:19,
+//     function(){
+//         return this.name,this.age
+//     }
+// }
+// console.log(detail);
+
+
+
+
+
+
+// maths in javascript
+
+// math M capital
+// absv= absoute means minus value plus
+// round means ex (4.6)=6 , 4.3=4
+// but my decison = 1st ceil(4.2)-5
+// 2nd floorvlue (3,2)-3
+// sqrt means number divide by same number
+// minvalue= 
+// maxvalue=
+// abs
+// console.log(Math);
+// console.log(Math.abs(-55));
+
+// const Math = -334
+// console.log(Math.abs(-334));
+
+// const num = new Math(-233)
+// console.log(Math.abs());
+
+
+// console.log(Math.floor(4.9));
+
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(9.1));
+
+
+
+
+// Math.random is mostly use in the math liberary
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.random()*10);
+
+// console.log(Math.random());
+// console.log(Math.ceil(Math.random()*10)+1);
+
+// const min = 14
+// const max = 19
+
+// console.log (Math.floor( Math.random() * (max - min + 1)) + max);
+
+
+// const min = 1
+// const max = 9
+
+// console.log(Math.floor(Math.random()* (max- min + 1))+ min)
